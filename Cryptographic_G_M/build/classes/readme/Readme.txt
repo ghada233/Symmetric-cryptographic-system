fdb94b5d0b60ab372aae4e28588e0cdb795a42a7
GhadaAljohani <70041510+ghada233@users.noreply.github.com>
2022-08-14 12:52:59 +0300

    Add files via upload

---
+++ Cryptographic_G_M/build/classes/readme/Readme.txt
@@ -0,0 +1,80 @@
+Ghadah Abdulaziz Aljohani
+4051065
+MUTYIAH,YASLAM,HAMAD,ALSAIARI
+4050906
+
+>In project :
+we have 3 folder one for test our programing and it is name
+"Folder_test"
+
+and we have folder readme 
+
+and one to run in project  
+
+ 
+>How to Run
+------------------------------------------------------
+1. Download the zip file and extract the zip file.
+2. Open Netbeans and then go to File->Open Project. Then browse the project path.
+3. From the project right click on " cryptographic_g_m" file and select "Run File"
+4. It will show Main menu on the console with Encrypt,Decrypt and exit .
+5. If user select  Encrypt/Decrypt option then it will ask for (1) File (2) Folder
+6. when enter file or folder you shoud writing dir for folder or file like 
+
+>file :
+C:\Users\hp\Documents\NetBeansProjects\Cryptographic_G_M\src\Folder_test\G&M.txt
+C:\Users\hp\Documents\NetBeansProjects\Cryptographic_G_M\src\Folder_test\Thanks.txt
+-----------------------------------------------------------------------------------
+>folder :
+C:\Users\hp\Documents\NetBeansProjects\Cryptographic_G_M\src\Folder_test
+-----------------------------------------------------------------------------------
+7. in our code we divid the programing in sub method to be easy to debug 
+8. if you choice a wrong option from 1- Encrypt, 2- Decrypt and 3-exit
+the programing will send to you notification about it like :
+ 
+" Invalid choice ):  Enter choice 1 to Encrypt  or 2 to Decrypt  or 3 to  Exit "
+
+9. if you choice a wrong option from (1) File (2) Folder
+the programing will send to you notification about it like :
+
+" Invalid choice ):   Enter choice 1 to File  or 2 to Folder  try  programing agin "
+
+
+
+>Reference:
+-----------------------------------------------------------------------------------
+https://www.youtube.com/watch?v=zn_kg55GRWo&t=341s
+https://www.baeldung.com/java-aes-encryption-decryption
+
+-----------------------------------------------------------------------------------
+
+run:
+
+ A SYMMETRIC CRYPTO SYSTEM
+======================================= 
+MAIN MENU 
+------------------------------------- 
+1. Encrypt 
+2. Decrypt 
+3. Exit 
+-----------------------------------------
+
+Enter your choice: 1
+(1) File (2) Folder 
+Enter your choice: 1
+Type your file name : C:\Users\hp\Documents\NetBeansProjects\Cryptographic_G_M\src\Folder_test\G&M.txt
+Choose the algorithm (AES, DES) : AES
+Enter key : aaaaaaaaaaaaaaaaaaaaaaaa
+Done! File G&M.txt is encrypted using AES - 192
+Output file is G&M.encrypted
+ A SYMMETRIC CRYPTO SYSTEM
+======================================= 
+MAIN MENU 
+------------------------------------- 
+1. Encrypt 
+2. Decrypt 
+3. Exit 
+-----------------------------------------
+
+Enter your choice: 3
+BUILD SUCCESSFUL (total time: 39 seconds)
---
+++ Cryptographic_G_M/src/cryptographic_g_m/Cryptographic_G_M.java
@@ -0,0 +1,303 @@
+/*
+ This repo contains Ghadah& MUTYIAH 👍 attempt to solve the project problem 
+
+Ghadah Abdulaziz Aljohani
+4051065
+MUTYIAH,YASLAM,HAMAD,ALSAIARI
+4050906
+*/
+
+
+package cryptographic_g_m;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.security.Key;
+import java.util.Scanner;
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.DESKeySpec;
+import javax.crypto.spec.SecretKeySpec;
+
+public class Cryptographic_G_M {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        MenuChoice(); //استدعاء جملة الطباعة الاساسية 
+        //----------------------//
+        Scanner input = new Scanner(System.in); //ادخال البيانات من المستخدم  
+        int choice;// تعريف قيم الاختسارات بناء على جملة الطباعه 
+
+        while (true) { // while loop
+            System.out.print("Enter your choice: ");
+            choice = input.nextInt(); // اختيار المستخدم
+            if (choice == 1 || choice == 2 || choice == 3) {
+                if (choice != 3) {
+                    if (choice == 1) {
+                        //ENC                       
+                        Encrypt_Option();
+                    } else {
+                        //DEC 
+                        Decrypt_Option();
+
+                    }//end else  
+                    //اعادة ادخال البيانات 
+                    MenuChoice();
+                } //نوقف  
+                else {
+                    break;
+                }
+            }//big if 
+            else {
+                //اذا اختيار المستخدم كان خاطئ
+                System.out.println(choice + " Invalid choice ): .\t Enter choice 1 to Encrypt \t or 2 to Decrypt \t or 3 to  Exit ");
+            }
+        }//while loop
+    }//main 
+
+    /////////////////////////////////////////////////////////
+    //This method is used to show main menu in the console
+    private static void MenuChoice() {
+        System.out.println(" A SYMMETRIC CRYPTO SYSTEM\n"
+                + "======================================= \n"
+                + "MAIN MENU \n"
+                + "------------------------------------- \n"
+                + "1. Encrypt \n"
+                + "2. Decrypt \n"
+                + "3. Exit \n"
+                + "-----------------------------------------\n");
+    }
+    /////////////////////////////////////////////////////////////
+
+    public static void Encrypt_Option() {
+        Scanner input = new Scanner(System.in); //عبارة عن اتاحه اخذ بيانات من المستخدم عشان يختار ملف اومجلد 
+        int option = 0;
+        String FileFolder = "";
+        String algorithm;
+        String key = "";
+
+        while (true) {
+            System.out.println("(1) File (2) Folder ");
+            System.out.print("Enter your choice: ");
+            option = input.nextInt();
+            if (option == 1 || option == 2) {
+
+                if (option == 1) {
+                    System.out.print("Type your file name : ");
+                    FileFolder = input.next();
+                    System.out.print("Choose the algorithm (AES, DES) : ");
+                    algorithm = input.next();
+                    System.out.print("Enter key : ");
+                    key = input.next();
+                    if (algorithm.equalsIgnoreCase("AES")) {
+                        AES_Encrypt(key, FileFolder);
+                    } else if (algorithm.equalsIgnoreCase("DES")) {
+                        DES_Encrypt(key, FileFolder);
+                    }
+                    break;
+                } else if (option == 2) {
+                    System.out.print("Type your Folder name : ");
+                    FileFolder = input.next();
+                    System.out.print("Choose the algorithm (AES, DES) : ");
+                    algorithm = input.next();
+                    System.out.print("Enter key : ");
+                    key = input.next();
+                    File dir = new File(FileFolder);
+                    for (File file : dir.listFiles()) {
+                        if (file.getName().endsWith("txt")) {
+                            if (algorithm.equalsIgnoreCase("AES")) {
+                                AES_Encrypt(key, file.getAbsolutePath());
+                            } else if (algorithm.equalsIgnoreCase("DES")) {
+                                DES_Encrypt(key, file.getAbsolutePath());
+                            }
+
+                        }
+                    }
+                    break;
+                }
+
+            } 
+            else if(option != 1 || option != 2) {
+                System.out.println("Invalid choice ):  \t  Enter choice 1 to File  \t or 2 to Folder  try agin \n");
+                Encrypt_Option();
+                System.out.println("");
+                break;
+            }
+
+        }
+    }// end Encrypt_Option
+
+    ///////////////////////////////////////////////////////////////////////////////////
+    public static void Decrypt_Option() {
+        Scanner input = new Scanner(System.in); //عبارة عن اتاحه اخذ بيانات من المستخدم 
+        int option = 0;
+        String FileFolder = "";
+        String algorithm;
+        String key = "";
+        while (true) {
+            System.out.println("(1) File (2) Folder  ");
+            System.out.print("Enter your choice: ");
+            option = input.nextInt();
+            if (option == 1 || option == 2) {
+
+                if (option == 1) {
+                    System.out.print("Type your file name : ");
+                    FileFolder = input.next();
+                    System.out.print("Choose the algorithm (AES, DES) : ");
+                    algorithm = input.next();
+                    System.out.print("Enter  key : ");
+                    key = input.next();
+                    if (algorithm.equalsIgnoreCase("AES")) {
+                        AES_Decrypt(key, FileFolder);
+                    } else if (algorithm.equalsIgnoreCase("DES")) {
+                        DES_Decrypt(key, FileFolder);
+                    }
+                    break;
+                } else if (option == 2) {
+                    System.out.print("Type your Folder name : ");
+                    FileFolder = input.next();
+                    System.out.print("Choose the algorithm (AES, DES) : ");
+                    algorithm = input.next();
+                    System.out.print("Enter  key : ");
+                    key = input.next();
+                    File dir = new File(FileFolder);
+                    for (File file : dir.listFiles()) {
+                        if (file.getName().endsWith("encrypted")) {
+                            if (algorithm.equalsIgnoreCase("AES")) {
+                                AES_Decrypt(key, file.getAbsolutePath());//
+                            } else if (algorithm.equalsIgnoreCase("DES")) {
+                                DES_Decrypt(key, file.getAbsolutePath());
+                            }
+                        }
+                    }
+                    break;
+                }
+
+            } else if(option != 1 || option != 2) {
+                System.out.println("Invalid choice ):  \t  Enter choice 1 to File  \t or 2 to Folder  try agin \n");
+                Decrypt_Option();
+                System.out.println("");
+                break;
+            }
+        }
+    }// end Decrypt_Option
+
+    ///////////////////////////////////////////////////////////////////////////////////
+    public static void AES_Encrypt(String key, String input) {
+        try {
+            File inFile = new File(input);
+            File outFile = new File(input.replace(".txt", ".encrypted"));
+            Key secretKey = new SecretKeySpec(key.getBytes(), 0, 24, "AES");
+            Cipher cipher = Cipher.getInstance("AES");
+            cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+
+            FileInputStream inputStream = new FileInputStream(inFile);
+            byte[] inputBytes = new byte[(int) inFile.length()];
+            inputStream.read(inputBytes);
+
+            byte[] outputBytes = cipher.doFinal(inputBytes);
+
+            FileOutputStream outputStream = new FileOutputStream(outFile);
+            outputStream.write(outputBytes);
+            outputStream.flush();
+            inputStream.close();
+            outputStream.close();
+
+            System.out.println("Done! File " + inFile.getName() + " is encrypted using AES - 192");
+            System.out.println("Output file is " + outFile.getName());
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+            ex.printStackTrace();
+        }
+    }
+
+    ///////////////////////////////////////////////////////////////////////////////////
+    public static void AES_Decrypt(String key, String input) {
+        try {
+            File inFile = new File(input);
+            File outFile = new File(inFile.getAbsolutePath().replace(".encrypted", ".decrypted"));
+            Key secretKey = new SecretKeySpec(key.getBytes(), 0, 24, "AES");
+            Cipher cipher = Cipher.getInstance("AES");
+            cipher.init(Cipher.DECRYPT_MODE, secretKey);
+            FileInputStream inputStream = new FileInputStream(inFile);
+            byte[] inputBytes = new byte[(int) inFile.length()];
+            inputStream.read(inputBytes);
+            byte[] outputBytes = cipher.doFinal(inputBytes);
+
+            FileOutputStream outputStream = new FileOutputStream(outFile);
+            outputStream.write(outputBytes);
+            outputStream.flush();
+            inputStream.close();
+            outputStream.close();
+
+            System.out.println("Done! File " + inFile.getName() + " is decrypted using AES - 192");
+            System.out.println("Output file is " + outFile.getName());
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+            ex.printStackTrace();
+        }
+    }
+
+    ///////////////////////////////////////////////////////////////////////////////////
+    public static void DES_Encrypt(String key, String input) {
+        try {
+            File inFile = new File(input);
+            File outFile = new File(input.replace(".txt", ".encrypted"));
+            DESKeySpec keySpec = new DESKeySpec(key.getBytes());
+            SecretKeyFactory keyFac = SecretKeyFactory.getInstance("DES");
+            SecretKey desKey = keyFac.generateSecret(keySpec);
+            Cipher cipher = Cipher.getInstance("DES");
+            cipher.init(Cipher.ENCRYPT_MODE, desKey);
+            FileInputStream inputStream = new FileInputStream(inFile);
+            byte[] inputBytes = new byte[(int) inFile.length()];
+            inputStream.read(inputBytes);
+            byte[] outputBytes = cipher.doFinal(inputBytes);
+            FileOutputStream stream = new FileOutputStream(outFile);
+            stream.write(outputBytes);
+            stream.flush();
+            inputStream.close();
+            stream.close();
+
+            System.out.println("Done! File " + inFile.getName() + " is Encrypted using DES");
+            System.out.println("Output file is " + outFile.getName());
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+            ex.printStackTrace();
+        }
+    }
+
+    ///////////////////////////////////////////////////////////////////////////////////
+    public static void DES_Decrypt(String key, String input) {
+        try {
+            File inFile = new File(input);
+            File outFile = new File(inFile.getAbsolutePath().replace(".encrypted", ".decrypted"));
+            DESKeySpec keySpec = new DESKeySpec(key.getBytes());
+            SecretKeyFactory keyFac = SecretKeyFactory.getInstance("DES");
+            SecretKey desKey = keyFac.generateSecret(keySpec);
+            Cipher cipher = Cipher.getInstance("DES");
+            cipher.init(Cipher.DECRYPT_MODE, desKey);
+
+            FileInputStream inputStream = new FileInputStream(inFile);
+            byte[] inputBytes = new byte[(int) inFile.length()];
+            inputStream.read(inputBytes);
+
+            byte[] outputBytes = cipher.doFinal(inputBytes);
+
+            FileOutputStream stream = new FileOutputStream(outFile);
+            stream.write(outputBytes);
+            stream.flush();
+            inputStream.close();
+            stream.close();
+
+            System.out.println("Done! File " + inFile.getName() + " is Decrypted using DES");
+            System.out.println("Output file is " + outFile.getName());
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+            ex.printStackTrace();
+        }
+    }
+    ///////////////////////////////////////////////////////////////////////////////////
+
+}//pakage
